813860277ca4a4ac7b7e7f1e2d4b7a9c707fa0cd
Doğukan Kökce <88978496+dgknkokce@users.noreply.github.com>
2023-06-25 10:30:33 +0000

    candidate registration added

--- voting_system.sol
+++ voting_system.sol
@@ -32,4 +32,23 @@ contract voting_system {
 
     //List of voter identity hashes
     bytes32 [] voters;
+
+
+    /**
+     * @dev This function is to register as a candidate for the elections
+     * It is public for anyone to register
+     * For this function to make sense we must::
+     * 1. Create the hash of the candidate data.
+     * 2. We store the hash of the candidate's data linked to their name.
+     * 3. We store the name of the candidate in the dynamic array candidates.
+     */
+
+    function candidateRegistration(string memory _candidateName, uint _candidateAge, string memory _candidateId) public {
+        //the current time plus 5 minutes
+        require(now<=(start_voting + 5 minutes), "Candidates can no longer be submitted");
+        bytes32 candidate_hash = keccak256(abi.encodePacked(_candidateName, _candidateAge, _candidateId));
+        candidate_ID[_candidateName] = candidate_hash;
+        candidates.push(_candidateName);     
+    }
+
 }
